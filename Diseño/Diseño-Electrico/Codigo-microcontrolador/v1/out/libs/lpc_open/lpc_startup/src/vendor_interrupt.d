ab2148f503bf153f9e0051a3fe07ce4349953a02
Pabloale96 <pabloale96@gmail.com>
2021-08-03 20:40:28 -0300

    organizacion de carpetas

---
+++ Diseño/Diseño-Electrico/Codigo-microcontrolador/v1/out/libs/lpc_open/lpc_startup/src/vendor_interrupt.d
@@ -0,0 +1,2 @@
+TP-Prof/Diseño/Diseño-Electrico/Codigo-microcontrolador/v1/out/libs/lpc_open/lpc_startup/src/vendor_interrupt.o: \
+ libs/lpc_open/lpc_startup/src/vendor_interrupt.c
---
+++ Diseño/Diseño-Electrico/Codigo-microcontrolador/v1/out/libs/sys_newlib/src/system.d
@@ -0,0 +1,2 @@
+TP-Prof/Diseño/Diseño-Electrico/Codigo-microcontrolador/v1/out/libs/sys_newlib//src/system.o: \
+ libs/sys_newlib//src/system.c
